c34a4ae6fe0dda877ce0d4fa222f00e447b48658
Simon Perdrisat <perdrisat@gmail.com>
2017-07-06 01:40:02 +0200

    change action title

--- background.js
+++ background.js
@@ -24,50 +24,52 @@ toogleTransition();
 browser.browserAction.onClicked.addListener(handleClick);
 browser.tabs.onUpdated.addListener(handeTabUpdate)
 
-function handleClick(){
-  playTransition = !playTransition;
-  toogleTransition();
+function handleClick() {
+    playTransition = !playTransition;
+    toogleTransition();
 }
 
-function handeTabUpdate(tabId, changeInfo, tabInfo){
-  if(playTransition == false){
-    disableTabTransition(tabId);
-  }
-}
-
-function toogleTransition(){
-    if(playTransition == false) {
-    disableAllTabsTransition();
-    browser.browserAction.setIcon({path: "icons/play.svg"});
-  } else{
-    enableAllTabsTransition();
-    browser.browserAction.setIcon({path: "icons/pause.svg"});
-  }
-}
-
-function disableAllTabsTransition(){
-  let gettingAllTabs = browser.tabs.query({});
-  gettingAllTabs.then((tabs) => {
-    for (let tab of tabs) {
-      disableTabTransition(tab);
+function handeTabUpdate(tabId, changeInfo, tabInfo) {
+    if (playTransition == false) {
+        disableTabTransition(tabId);
     }
-  });
 }
 
-function disableTabTransition(tab){
-  browser.tabs.insertCSS(tab.id, {code: CSS});
-}
-
-function enableAllTabsTransition(){
-  let gettingAllTabs = browser.tabs.query({});
-  gettingAllTabs.then((tabs) => {
-    for (let tab of tabs) {
-      enableTabTransition(tab);
+function toogleTransition() {
+    if (playTransition == false) {
+        disableAllTabsTransition();
+        browser.browserAction.setIcon({ path: "icons/play.svg" });
+        browser.browserAction.setTitle({ title: 'Restore annimations on all tabs' });
+    } else {
+        enableAllTabsTransition();
+        browser.browserAction.setIcon({ path: "icons/pause.svg" });
+        browser.browserAction.setTitle({ title: 'Pause annimations on all tabs' });
     }
-  });
+}
+
+function disableAllTabsTransition() {
+    let gettingAllTabs = browser.tabs.query({});
+    gettingAllTabs.then((tabs) => {
+        for (let tab of tabs) {
+            disableTabTransition(tab);
+        }
+    });
+}
+
+function disableTabTransition(tab) {
+    browser.tabs.insertCSS(tab.id, { code: CSS });
+}
+
+function enableAllTabsTransition() {
+    let gettingAllTabs = browser.tabs.query({});
+    gettingAllTabs.then((tabs) => {
+        for (let tab of tabs) {
+            enableTabTransition(tab);
+        }
+    });
 }
 
 
-function enableTabTransition(tab){
-  browser.tabs.removeCSS(tab.id, {code: CSS});
+function enableTabTransition(tab) {
+    browser.tabs.removeCSS(tab.id, { code: CSS });
 }
